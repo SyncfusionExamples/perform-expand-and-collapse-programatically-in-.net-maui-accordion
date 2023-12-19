ea7dcafb53f9deda6fe4259eb9631f297887a8e6
RavikumarSF4250 <147583109+RavikumarSF4250@users.noreply.github.com>
2023-12-19 16:36:32 +0530

    Added Sample for Expand and Collapse Programatically in Maui

---
+++ AccordionMaui/AccordionMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AccordionMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AccordionMaui/AccordionMaui/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AccordionMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
